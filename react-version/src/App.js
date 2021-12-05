6f8340e2bae94c78e6055251e2df996945820f88
Muaadahmed <muaad.abdi.ahmed@gmail.com>
2021-12-05 14:50:39 -0500

    Import all components to app.js

--- react-version/src/App.js
+++ react-version/src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Navigation from './components/Navigation'
+import Profile from './components/Profile'
+import TweetList from './components/TweetList'
+import TweetForm from './components/TweetForm'
 
 function App() {
   return (
